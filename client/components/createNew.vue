3b50e2a46b873a5c35b2f47cd373383d67510f37
Yusei Ito <yuseikun@Outlook.jp>
2020-03-21 21:10:57 +0900

    Use generic modal

---
+++ client/components/createNew.vue
@@ -0,0 +1,42 @@
+<template>
+<modal v-bind:value="value" @input="updateValue()">
+      <input type="text" class="input" placeholder="event title">
+       <VueCtkDateTimePicker v-model="datetime_model" />
+       <textarea class="textarea is-primary" placeholder="Note"></textarea>
+       <div v-bind:style="{height:(isMobile?'20vh':'100vh')}"></div>
+</modal>
+</template>
+<script>
+import modal from "./modal.vue"
+import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
+import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
+export default{
+    name:"createNew",
+    props: {
+        value:Boolean
+    },
+    data: function(){
+        return {
+            isMobile:false,
+            datetime_model:null
+        }
+    },
+    components:{
+        modal,
+        VueCtkDateTimePicker
+    },
+    updated:function(){
+            const ua = navigator.userAgent;
+            if (window.matchMedia('(max-width: 1200px)').matches) {
+            this.isMobile=true;
+            }
+    },
+    methods:{
+        updateValue:function(e){
+            this.$emit('input',e)
+        }
+    }
+}
+ </script>
+<style>
+</style>
--- client/components/ntaro_headder.vue
+++ client/components/ntaro_headder.vue
@@ -2,60 +2,25 @@
 <div>
 <header style="margin:1em;">
     <h1 class="title is-1 centre top-title">7組太郎</h1>
-    <button type="button" class="button is-primary is-light centre" v-on:click="flip">通知の登録はここから</button>
+    <button type="button" class="button is-primary is-light centre" v-on:click="modal_flg=true;">通知の登録はここから</button>
+    <createNew v-model="modal_flg"/>
 </header>
-<div v-bind:class="[isActive ? 'is-active' : '','modal']">
-  <div class="modal-background"></div>
-  <div class="modal-card">
-    <header class="modal-card-head">
-      <p class="modal-card-title">New Notification</p>
-      <button class="delete" aria-label="close" v-on:click="flip"></button>
-    </header>
-    <section class="modal-card-body">
-      <input type="text" class="input" placeholder="event title">
-       <VueCtkDateTimePicker v-model="datetime_model" />
-       <textarea class="textarea is-primary" placeholder="Note"></textarea>
 
-       <div v-bind:style="{height:(isMobile?'20vh':'100vh')}"></div>
-
-    </section>
-    <footer class="modal-card-foot">
-      <button class="button is-success" v-on:click="save">Save changes</button>
-      <button class="button">Cancel</button>
-    </footer>
-  </div>
-</div>
 </div>
 </template>
 <script>
-
-import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
-import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
-
+import createNew from "./createNew"
 export default{
-    name:"ntaro_headder",
-    components:{
-      VueCtkDateTimePicker
-    },
-    data:()=>{
-        return {isActive:false,datetime_model:null,isMobile:false}
-    },
-    updated:function(){
-        const ua = navigator.userAgent;
-         if (window.matchMedia('(max-width: 1200px)').matches) {
-           this.isMobile=true;
-        }
-    },
-    methods:{
-    flip:function(event){
-      this.isActive=!this.isActive;
-    },
-    save:function(event){
-      alert(this.datetime_model);
-      this.isActive=!this.isActive;
-    }}
+name:"ntaro_headder",
+components:{
+createNew
+},
+data: () => {
+    return {
+      modal_flg:false
+    }
 }
-
+}
 
 </script>
 
