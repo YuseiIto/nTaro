500bf06dcd1855b478d2dcbf17e12fcc85acac26
Yusei Ito <yuseikun@Outlook.jp>
2020-03-21 16:05:24 +0900

    Add:modal

--- client/components/ntaro_headder.vue
+++ client/components/ntaro_headder.vue
@@ -1,15 +1,40 @@
 <template>
-<header>
+<div>
+<header style="margin:1em;">
     <h1 class="title is-1 centre top-title">7組太郎</h1>
-    <h3 class="title is-5 centre">通知の登録はここから</h3>
+    <button type="button" class="button is-primary is-light centre" v-on:click="flip">通知の登録はここから</button>
 </header>
+<div v-bind:class="[isActive ? 'is-active' : '','modal']">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Modal title</p>
+      <button class="delete" aria-label="close" v-on:click="flip"></button>
+    </header>
+    <section class="modal-card-body">
+      <!-- Content ... -->
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-success">Save changes</button>
+      <button class="button">Cancel</button>
+    </footer>
+  </div>
+</div>
+</div>
 </template>
 <script>
 export default{
     name:"ntaro_headder",
     data:()=>{
-    }
+        return {isActive:false}
+    },
+    methods:{
+    flip:function(event){
+      this.isActive=!this.isActive;
+    }}
 }
+
+
 </script>
 
 <style>
@@ -18,6 +43,6 @@ export default{
 }
 
 .centre{
-     text-align: center;
+       margin-bottom: 0.8em;
 }
 </style>
