4f6d780e5daa5cf2a69aeda84a5928d6ad9fd17b
Yusei Ito <yuseikun@Outlook.jp>
2020-03-21 13:51:16 +0900

    Initial vue setup

---
+++ client/components/head_logo.vue
@@ -0,0 +1,11 @@
+<template>
+<h1>{{greeting}}</h1>
+</template>
+<script>
+export default{
+    name:"head_logo",
+    data:()=>{
+        return {greeting:"Hello"}
+    }
+}
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,8 +1,15 @@
 <template>
+<div>
 <h1>title</h1>
+<head_logo/>
+</div>
 </template>
 <script>
+import head_logo from "../components/head_logo.vue"
 export default {
+  components:{
+      head_logo
+  },
   head() {
     return {
       title: 'index'
