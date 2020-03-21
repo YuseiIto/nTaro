f8b697113fc17fbcf720da8bb12bda39905b74ab
Yusei Ito <yuseikun@Outlook.jp>
2020-03-21 16:05:33 +0900

    Add: basic tiles

---
+++ client/components/list.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="tile is-vertical is-12">
+    <div class="tile">
+      <div class="tile is-parent is-vertical">
+        <article class="tile is-child notification is-primary">
+          <p class="title">Vertical...</p>
+          <p class="subtitle">Top tile</p>
+        </article>
+        <article class="tile is-child notification is-warning">
+          <p class="title">...tiles</p>
+          <p class="subtitle">Bottom tile</p>
+        </article>
+      </div>
+    </div>
+</div>
+</template>
+<script>
+export default{
+    name:"list",
+    data:()=>{
+        return {
+          
+        }
+    },
+    props: {
+      count: Number
+    }
+}
+</script>
+
+<style>
+.control{
+  margin-top:10px;
+}
+</style>
