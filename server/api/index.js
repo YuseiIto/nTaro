fd31f8c5cabe535dd07c8d0f51990a1c81099700
Yusei Ito <yuseikun@Outlook.jp>
2020-03-22 00:34:43 +0900

    Add /get entrypoint

--- server/api/index.js
+++ server/api/index.js
@@ -3,8 +3,10 @@ const router = Router()
 
 import add from "./add"
 import remove from "./remove"
+import get from "./get"
 
 router.use("/add", add)
 router.use("/remove", remove)
+router.use("/get", get)
 
 export default router
