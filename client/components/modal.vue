055c8d4784327dd3b2bc2c17db968c423f8c02b6
Yusei Ito <yuseikun@Outlook.jp>
2020-03-21 21:10:40 +0900

    Create: generic modal element

---
+++ client/components/modal.vue
@@ -0,0 +1,38 @@
+<template>
+<div v-bind:class="[value ? 'is-active' : '','modal']">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">New Notification</p>
+      <button class="delete" aria-label="close" v-on:click="close"></button>
+    </header>
+    <section class="modal-card-body">
+        <slot></slot>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-success" v-on:click="save">Save changes</button>
+      <button class="button">Cancel</button>
+    </footer>
+  </div>
+</div>
+</template>
+<script>
+
+
+
+export default{
+    name:"modal",
+    props:{
+        value:Boolean
+    },
+    methods:{
+    save:function(){
+      this.$emit('input',this.value);
+    },
+    close:function(){
+        this.$emit('input',false);
+    }}
+
+}
+
+</script>
