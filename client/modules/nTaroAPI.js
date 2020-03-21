fe3e98a46e54fdf5a85974aabf47b74eb1d72635
Yusei Ito <yuseikun@Outlook.jp>
2020-03-22 00:46:21 +0900

    Remove functions, add methods

--- client/modules/nTaroAPI.js
+++ client/modules/nTaroAPI.js
@@ -1,6 +1,17 @@
-export function remove(id, callback) {
-    console.log(`Hello${id}`)
+import axios from "axios";
+
+export function ntaroRemove(id, callback) {
+    axios.post("/remove", { id: id })
     if (typeof(callback) == "function") {
         callback();
     }
 };
+
+export async function ntaroGet() {
+    const res = await axios.get("/get")
+    return res.data;
+};
+
+export function ntaroAdd(name, datetime, content) {
+    axios.post("/add", { name: name, datetime: datetime, content: content })
+}
