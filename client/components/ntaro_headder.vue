78925401633bc1fadd6306cb84e4318709eb88ba
Yusei Ito <yuseikun@Outlook.jp>
2020-03-22 16:34:19 +0900

    Listen change event and cause change event

--- client/components/ntaro_headder.vue
+++ client/components/ntaro_headder.vue
@@ -3,7 +3,7 @@
 <header style="margin:1em;">
     <h1 class="title is-1 centre top-title">7組太郎</h1>
     <button type="button" class="button is-primary is-light centre" v-on:click="modal_flg=true;">通知の登録はここから</button>
-    <createNew v-model="modal_flg"/>
+    <createNew v-model="modal_flg" v-on:change="$emit('change')"/>
 </header>
 
 </div>
