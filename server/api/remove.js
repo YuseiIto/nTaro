7aa7614e6f4cc292e5271fa587bfa779e51e270d
Yusei Ito <yuseikun@Outlook.jp>
2020-03-22 17:19:36 +0900

    Modify: Remove from db

--- server/api/remove.js
+++ server/api/remove.js
@@ -1,11 +1,10 @@
 import { Router } from "express"
-
+import { deleteRecord } from "./db"
 const router = Router()
 
 /* GET users listing. */
 router.post("/", (req, res) => {
-    console.log(`New request. ID:${req.body.id}`)
-    res.send("respond wiiiiith a resource")
+    deleteRecord(req.body.id).then(() => { res.send({ res: "OK" }) });
 })
 
 export default router
