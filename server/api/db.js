b64607a0cf4e572350924623fe4783b0b95595f5
Yusei Ito <yuseikun@Outlook.jp>
2020-03-22 14:55:26 +0900

    Modify: add db validation and add function

--- server/api/db.js
+++ server/api/db.js
@@ -1,14 +1,44 @@
 import mongodb from 'mongodb'
 const MongoClient = mongodb.MongoClient
+const task_col_name = "tasks"
+const db_name = "test"
 
 if (process.env.NODE_ENV != "production") {
-    require('dotenv').config();
+    require('dotenv').config()
 }
 
-const uri = process.env.MONGODB_URI;
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ntaro-uohel.mongodb.net/${db_name}?retryWrites=true&w=majority`
 
-
+/*
+ * MogoDB validation and setup.
+ */
 MongoClient.connect(uri, (err, db) => {
-    console.log(`Connected successfully to server on ${uri}`)
-    db.close()
-})
+
+    const dbo = db.db(db_name);
+    dbo.listCollections().toArray().then((arr) => {
+        if (arr.findIndex(({ name }) => name === task_col_name) > -1) {
+            console.log(`Collection "${task_col_name}" exists!`);
+            db.close()
+        } else {
+            console.log(`Collection "${task_col_name}" doesn't exists!`);
+            dbo.createCollection(task_col_name).then(() => {
+                console.log("Collection created");
+                db.close()
+            })
+        }
+
+    });
+});
+
+export function addRecord(obj) {
+    return new Promise(resolve => {
+        MongoClient.connect(uri, (err, db) => {
+            const dbo = db.db(db_name);
+            const tasks = dbo.collection(task_col_name);
+            tasks.insertOne(obj)
+            db.close();
+            resolve();
+            return;
+        })
+    });
+}
