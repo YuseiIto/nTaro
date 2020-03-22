f0817bdf2ba0aef180eee621849f830617ad812d
Yusei Ito <yuseikun@Outlook.jp>
2020-03-22 16:14:40 +0900

    Chaneg id series

--- client/components/list.vue
+++ client/components/list.vue
@@ -2,10 +2,10 @@
 <div class="tile is-vertical is-12">
     <div class="tile">
       <div class="tile is-parent is-vertical">
-        <card v-for="item in records" :key="item.id" :title="item.name"  :datetime="item.datetime" :content="item.content" :id=item.id v-on:click="promptRemove()" v-on:remove="delFnc(item.id)" ></card>
+        <card v-for="(item,index) in records" :key="item.id" :title="item.name"  :datetime="item.datetime" :content="item.content" :id="index+1" v-on:click="promptRemove" v-on:remove="delFnc(item.id)" ></card>
       </div>
     </div>
-    <askRemove v-model="modal" />
+    <askRemove v-model="modal" :message="delMsg" />
     </div>
 </template>
 <script>
@@ -22,18 +22,20 @@ export default{
     data:function(){
         return {
                 records:[],
-                modal:false
+                modal:false,
+                delMsg:""
         }
     },
     created:async function(){
         this.records=await ntaroGet();
     },
     methods:{
-      promptRemove:function(e){
+      promptRemove:function(name){
         this.modal=true;
+        this.delMsg=name
       },
       delFnc:async function(id){
-        ntaroRemove(id);
+        ntaroRemove(this.records[id-1]);
         this.records=await ntaroGet();
       }
     }
