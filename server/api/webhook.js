b65b4d978e60c710ce59ee71b28949f112cf05b7
Yusei Ito <yuseikun@Outlook.jp>
2020-03-23 17:34:08 +0900

    Basic echo bot

--- server/api/webhook.js
+++ server/api/webhook.js
@@ -1,3 +1,25 @@
-export default function() {
+import line from "@line/bot-sdk";
 
+const client = new line.Client(config);
+
+async function bot_body(ev) {
+    const pro = await client.getProfile(ev.source.userId);
+    return client.replyMessage(ev.replyToken, {
+        type: "text",
+        text: `${pro.displayName} said"${ev.message.text}".`
+    })
+}
+
+
+export default function(req, res) {
+    res.status(200).end();
+    const events = req.body.events;
+    const promises = [];
+    for (let i = 0, l = events.length; i < l; i++) {
+        const ev = events[i];
+        promises.push(
+            bot_body(ev)
+        );
+    }
+    Promise.all(promises).then(console.log("pass"));
 }
