ff324a33c1175d5e7b8b9269b65a20137f90ccf7
Yusei Ito <yuseikun@Outlook.jp>
2020-03-23 17:29:13 +0900

    Modify: LINE bot Initial setup

--- server/api/index.js
+++ server/api/index.js
@@ -5,8 +5,22 @@ import add from "./add"
 import remove from "./remove"
 import get from "./get"
 
+import line from "@line/bot-sdk";
+import lineBot from "./webhook";
+
+if (process.env.NODE_ENV != "production") {
+    require('dotenv').config()
+}
+
+const config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_SECRET_KEY
+};
+
+
 router.use("/add", add)
 router.use("/remove", remove)
 router.use("/get", get)
+router.post("/webhook", line.middleware(config), (req, res) => lineBot(req, res))
 
 export default router
